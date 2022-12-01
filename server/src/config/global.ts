d3b1e6d69586da0de92675e1a22791a7ca1ff46e
v_janyzheng <v_janyzheng@tencent.com>
2022-12-01 10:12:50 +0800

    feat: 解决sever端口冲突

--- server/src/config/global.ts
+++ server/src/config/global.ts
@@ -3,6 +3,6 @@ import { registerAs } from '@nestjs/config';
 const { env } = process;
 
 export default registerAs('global', () => ({
-    ip: env.HOST || '8888',
+    ip: env.HOST || '9999',
     port: env.PORT || '127.0.0.1',
 }));
