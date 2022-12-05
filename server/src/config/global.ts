a2ac26bbe06abba5a3a724ffeb3c464c2fc6fcba
v_janyzheng <v_janyzheng@tencent.com>
2022-12-05 10:25:10 +0800

    feat: host database rename

--- server/src/config/global.ts
+++ server/src/config/global.ts
@@ -3,6 +3,6 @@ import { registerAs } from '@nestjs/config';
 const { env } = process;
 
 export default registerAs('global', () => ({
-    ip: env.HOST || '9999',
-    port: env.PORT || '127.0.0.1',
+  ip: env.HOST || '9003',
+  port: env.PORT || '127.0.0.1',
 }));
